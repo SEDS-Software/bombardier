3de7e05eff8decf72ce93e8d904c59f8b244c1ad
Holden Adamec <holden.adamec@gmail.com>
2021-02-14 10:39:48 -0800

    Removed oop cuz this is python, not java

--- ground_station.py
+++ ground_station.py
@@ -1,24 +1,16 @@
 import tkinter as tk
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        master.title("Ground Station v1.0.0")
-        master.iconbitmap("./logo.ico")
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.abort_button = tk.Button(self)
-        self.abort_button["text"] = "ABORT"
-        self.abort_button["command"] = self.abort
-        self.abort_button["bg"] = "red"
-        self.abort_button.pack()
-
-    def abort(self):
-        print("Aborting Launch")
+def abort():
+    print("Aborting Launch")
 
 root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+root.title("Ground Station v1.0.0")
+root.iconbitmap("./logo.ico")
+
+abort_button = tk.Button(root)
+abort_button["text"] = "ABORT"
+abort_button["command"] = abort
+abort_button["bg"] = "red"
+abort_button.pack()
+
+root.mainloop()
