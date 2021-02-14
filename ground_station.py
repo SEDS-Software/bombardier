3000ae81b3b548d39a16605b6191261cb6f43285
Holden Adamec <holden.adamec@gmail.com>
2021-02-14 10:48:18 -0800

    Added valve statuses frame

--- ground_station.py
+++ ground_station.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 
+valve_states = [
+	("Main Valve", 1),
+	("Vent Valve", 1),
+	("Fuel Fill Valve", 0)
+]
+
 def abort():
     print("Aborting Launch")
 
@@ -7,10 +13,12 @@ root = tk.Tk()
 root.title("Ground Station v1.0.0")
 root.iconbitmap("./logo.ico")
 
-abort_button = tk.Button(root)
-abort_button["text"] = "ABORT"
-abort_button["command"] = abort
-abort_button["bg"] = "red"
+abort_button = tk.Button(root, text="ABORT", bg="red", command=abort)
 abort_button.pack()
+
+valve_status_frame = tk.LabelFrame(root, text="Valve Statuses")
+for name, state in valve_states:
+	tk.Label(valve_status_frame, text=name).pack()
+valve_status_frame.pack()
 
 root.mainloop()
